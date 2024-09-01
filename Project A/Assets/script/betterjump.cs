4e3a4e39da4e1f07d3687e00ffa8e2d40ffed4fc
chetsadaphon24 <114966348+chetsadaphon24@users.noreply.github.com>
2024-09-02 02:22:50 +0700

    camera

--- Project A/Assets/script/betterjump.cs
+++ Project A/Assets/script/betterjump.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class betterjump : MonoBehaviour
 {
     private Rigidbody2D rb;
-    public float jumpVelocity = 5;
     public float fallMultiplier = 2.5f;
     public float lowJumpMultiplier = 2f;
 
@@ -18,10 +17,6 @@ public class betterjump : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Jump"))
-        {
-            GetComponent<Rigidbody2D>().velocity = Vector2.up * jumpVelocity;
-        }
 
         if (rb.velocity.y < 0)
         {
